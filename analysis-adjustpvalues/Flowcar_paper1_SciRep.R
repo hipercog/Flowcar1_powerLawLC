e3dc6e82f55b69ab564a312662afdc84f7249243
zenBen <benjamin.cowley@ttl.fi>
2019-02-04 10:17:47 +0200

    Tweaked R.

--- analysis-adjustpvalues/Flowcar_paper1_SciRep.R
+++ analysis-adjustpvalues/Flowcar_paper1_SciRep.R
@@ -1,11 +1,20 @@
-library(tidyverse)
-library(ggplot2)
-library(lmSupport)
-library(jtools)
+setwd("/home/ben/Benslab/FLOW/papers/Flowcar1_powerLawLC/analysis-adjustpvalues") # convenience line
 
-setwd("/home/ben/Benslab/FLOW/extra_analyses/Flowcar_paper1_SciRep") # convenience line
-
+padj <- padj[1:12,]
 
 padj$padj <- p.adjust(padj$pvals, method = "holm")
 print(padj)
 
+# %           tests  pvals   padj
+# % 1       LCxFlow 0.0800 0.6400
+# % 2      FlowXssn 0.7700 1.0000
+# % 3  FlowXssn1dur 0.9000 1.0000
+# % 4  FlowXssn2dur 0.9000 1.0000
+# % 5  FlowXssn3dur 0.0300 0.3000
+# % 6  FlowXssn4dur 0.0200 0.2200
+# % 7  FlowXssn5dur 0.9000 1.0000
+# % 8  FlowXssn6dur 0.0300 0.3000
+# % 9  FlowXssn7dur 0.9000 1.0000
+# % 10 FlowXssn8dur 0.9000 1.0000
+# % 11 FlowXssn9dur 0.9000 1.0000
+# % 12     FlowXdev 0.0002 0.0024
